8e9d542dbfe4564799f914f58c4e73f27f22fcef
Priyansh Yadav <115138928+Priyanshx10@users.noreply.github.com>
2023-10-11 03:58:42 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,6 +4,12 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
